cb3042b6aaa35c385ba1adec4b45fd254d268e69
wangxf2000 <wangxf.2000@gmail.com>
2020-03-13 11:06:03 +0800

    Update setup.sh
    update mqtt link

--- setup.sh
+++ setup.sh
@@ -197,7 +197,7 @@ systemctl start mosquitto
 
 echo "--Download the NiFi MQTT Processor to read from mosquitto"
 mkdir -p /opt/cloudera/cem/minifi/lib/
-wget http://central.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
+wget https://repo1.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
 chown root:root /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
 chmod 660 /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
 
