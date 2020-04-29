b5dc8ebcc7ac4ee0eeb5064ffd25c4869fd395cd
wangxf2000 <wangxf.2000@gmail.com>
2020-04-29 18:40:33 +0800

    Update create_cluster.py

--- scripts/create_cluster.py
+++ scripts/create_cluster.py
@@ -88,6 +88,7 @@ with open(sys.argv[1]) as f:
 
 Response = namedtuple("Response", "data")
 dst_cluster_template=api_client.deserialize(response=Response(json_str),response_type=cm_client.ApiClusterTemplate)
+print("Starting to import Cluster template now...")
 cmd = cm_api.import_cluster_template(add_repositories=True, body=dst_cluster_template)
 wait(cmd)
-
+print("Finished to import Cluster template now...")
