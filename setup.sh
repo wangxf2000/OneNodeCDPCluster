ecfddf71c2a401ae20ca8402d7691d2d48760030
wangxf2000 <wangxf.2000@gmail.com>
2020-08-21 18:18:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -169,18 +169,19 @@ chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
 
 echo "-- Install CEM Tarballs"
 mkdir -p /opt/cloudera/cem
-wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
-ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
-ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
+wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.2.0.0/CEM-1.2.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM-1.2.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.2.0.0-70/tars/efm/efm-1.0.0.1.2.0.0-70-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.2.0.0-70/tars/minifi/minifi-0.6.0.1.2.0.0-70-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.2.0.0-70/tars/minifi/minifi-toolkit-0.6.0.1.2.0.0-70-bin.tar.gz -C /opt/cloudera/cem
+rm -f /opt/cloudera/cem/CEM-1.2.0.0-centos7-tars-tarball.tar.gz
+ln -s /opt/cloudera/cem/efm-1.0.0.1.2.0.0-70 /opt/cloudera/cem/efm
+ln -s /opt/cloudera/cem/minifi-0.6.0.1.2.0.0-70 /opt/cloudera/cem/minifi
 ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
-chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.2.0.0-70
+chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.2.0.0-70
+chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.2.0.0-70
+
 rm -f /opt/cloudera/cem/efm/conf/efm.properties
 cp conf/efm.properties /opt/cloudera/cem/efm/conf
 rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
