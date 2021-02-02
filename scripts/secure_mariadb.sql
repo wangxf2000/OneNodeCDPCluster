3f0359ac86c99b2c53154cf177c2094b30cfd624
wangxf2000 <wangxf.2000@gmail.com>
2021-02-02 16:01:06 +0800

    Update secure_mariadb.sql

--- scripts/secure_mariadb.sql
+++ scripts/secure_mariadb.sql
@@ -1,3 +1,6 @@
+SET GLOBAL innodb_file_per_table = ON,
+           innodb_file_format = Barracuda,
+           innodb_large_prefix = ON;
 UPDATE mysql.user SET Password=PASSWORD('cloudera') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
