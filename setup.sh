87fd08ccf8a8403d3a286663e9d03c56273d4112
wangxf2000 <wangxf.2000@gmail.com>
2021-04-18 11:46:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -153,7 +153,8 @@ EOF
 echo "-- Install CSDs"
 wget https://archive.cloudera.com/p/CFM/2.x/redhat7/yum/tars/parcel/NIFI-1.11.4.2.0.4.0-80.jar  -P /opt/cloudera/csd/
 wget https://archive.cloudera.com/p/CFM/2.x/redhat7/yum/tars/parcel/NIFIREGISTRY-0.6.0.2.0.4.0-80.jar  -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/p/csa/1.2.0.0/csd/FLINK-1.10.0-csa1.2.0.0-cdh7.1.1.0-565-3525501.jar -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/p/csa/1.3.0.0/csd/FLINK-1.12-csa1.3.0.0-cdh7.1.6.0-297-11607198.jar  -P /opt/cloudera/csd/
+wget https://archive.cloudera.com/p/csa/1.3.0.0/csd/SQL_STREAM_BUILDER-1.12-csa1.3.0.0-cdh7.1.6.0-297-11607198.jar   -P /opt/cloudera/csd/
 wget https://archive.cloudera.com/p/cdsw1/1.9.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDPDC-1.9.1.jar -P /opt/cloudera/csd/
 
 
