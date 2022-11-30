8a7e2724326133cd86de2763679b91ee0bf8fa73
wangxf2000 <wangxf.2000@gmail.com>
2022-11-30 15:33:20 +0800

    Update pgsql_create_db.sql

--- scripts/pgsql_create_db.sql
+++ scripts/pgsql_create_db.sql
@@ -1,9 +1,6 @@
 CREATE ROLE scm LOGIN PASSWORD 'cloudera';
 CREATE DATABASE scm OWNER scm ENCODING 'UTF8';
 
-CREATE ROLE amon LOGIN PASSWORD 'cloudera';
-CREATE DATABASE amon OWNER amon ENCODING 'UTF8';
-
 CREATE ROLE rman LOGIN PASSWORD 'cloudera';
 CREATE DATABASE rman OWNER rman ENCODING 'UTF8';
 
@@ -12,12 +9,6 @@ CREATE DATABASE hue OWNER hue ENCODING 'UTF8';
 
 CREATE ROLE hive LOGIN PASSWORD 'cloudera';
 CREATE DATABASE metastore OWNER hive ENCODING 'UTF8';
-
-CREATE ROLE nav LOGIN PASSWORD 'cloudera';
-CREATE DATABASE nav OWNER nav ENCODING 'UTF8';
-
-CREATE ROLE navms LOGIN PASSWORD 'cloudera';
-CREATE DATABASE navms OWNER navms ENCODING 'UTF8';
 
 CREATE ROLE oozie LOGIN PASSWORD 'cloudera';
 CREATE DATABASE oozie OWNER oozie ENCODING 'UTF8';
@@ -29,3 +20,24 @@ ALTER DATABASE metastore SET standard_conforming_strings=off;
 ALTER DATABASE oozie SET standard_conforming_strings=off;
 
 
+CREATE ROLE efm LOGIN PASSWORD 'cloudera';
+CREATE DATABASE efm OWNER efm ENCODING 'UTF8';
+
+CREATE ROLE nifireg LOGIN PASSWORD 'cloudera';
+CREATE DATABASE nifireg OWNER nifireg ENCODING 'UTF8';
+
+CREATE ROLE registry LOGIN PASSWORD 'cloudera';
+CREATE DATABASE registry OWNER registry ENCODING 'UTF8';
+
+
+CREATE ROLE streamsmsgmgr LOGIN PASSWORD 'cloudera';
+CREATE DATABASE streamsmsgmgr OWNER streamsmsgmgr ENCODING 'UTF8';
+
+CREATE ROLE rangerkms LOGIN PASSWORD 'cloudera';
+CREATE DATABASE rangerkms OWNER rangerkms ENCODING 'UTF8';
+
+CREATE ROLE ssb_admin LOGIN PASSWORD 'cloudera';
+CREATE DATABASE ssb_admin OWNER ssb_admin ENCODING 'UTF8';
+
+CREATE ROLE ssb_mve LOGIN PASSWORD 'cloudera';
+CREATE DATABASE ssb_mve OWNER ssb_mve ENCODING 'UTF8';
